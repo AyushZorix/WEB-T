be1c26ed615d225c6ef442f2e7ad911d7d3ab26b
AyushZorix <ayushbhandaristudy@gmail.com>
2026-01-09 14:58:21 +0530

    done

--- n2/New folder 2/src/components/nav.jsx
+++ n2/New folder 2/src/components/nav.jsx
@@ -5,12 +5,12 @@ import { NavLink, useNavigate } from "react-router-dom";
 export default function Nav() {
     const navigate = useNavigate();
     return (
-        <header className="bg-white shadow-sm">
+        <header className="bg-black shadow-sm">
             <div className="max-w-4xl mx-auto flex items-center justify-between p-4">
-                <h1 className="text-lg font-bold cursor-pointer" onClick={() => navigate("/")}>MyWebsite</h1>
+                <h1 className="text-lg font-bold cursor-pointer text-white" onClick={() => navigate("/")}>MyWebsite</h1>
                 <nav className="space-x-4">
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'underline font-semibold' : 'text-gray-600 hover:underline'} end>Home</NavLink>
-                    <NavLink to="/about_us" className={({ isActive }) => isActive ? 'underline font-semibold' : 'text-gray-600 hover:underline'} end>
+                    <NavLink to="/" className={({ isActive }) => isActive ? 'underline font-semibold text-white' : 'text-gray-300 hover:underline hover:text-white'} end>Home</NavLink>
+                    <NavLink to="/about_us" className={({ isActive }) => isActive ? 'underline font-semibold text-white' : 'text-gray-300 hover:underline hover:text-white'} end>
                     About Us
                     </NavLink>
                     
@@ -19,4 +19,5 @@ export default function Nav() {
         </header>
         
     
-    );}
+    );
+}
